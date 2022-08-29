b94246af823d6a67834d275b31ac075f1b35f866
luizfk <luiz.kessler7@gmail.com>
2022-08-29 13:31:56 -0300

    variáveis

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let nome = prompt("Nome")
+let idade = prompt("Qual sua idade")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log(`Olá ${nome} você tem ${idade} anos.`) 
+
+//D)Foi impresso com undefined, pois não foi atribuido nenhum valor.
+//F)Foi impresso os dois como string mesmo pedindo (idade), que é um valor de número.
---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+const perg1 = "Está gostando do curso?"
+const resp1 = prompt(perg1)
+const perg2 = "Está gostando de programar?"
+const resp2 = prompt(perg2)
+const perg3 = "Está muito difícil programar?"
+const resp3 = prompt(perg3)
+
+console.log(`${perg1} - ${resp1}`)
+console.log(`${perg2} - ${resp2}`)
+console.log(`${perg3} - ${resp3}`)
